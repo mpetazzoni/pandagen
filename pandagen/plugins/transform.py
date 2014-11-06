ab84bb5095fd9ab48c94f3114856568e7c8c8101
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2014-11-06 13:36:34 -0800

    Markdown parsing improvements
    * Add a CSS class to all content images
    * Add Markdown Smartypants for automatic typographic fixups
    
    Signed-off-by: Maxime Petazzoni <maxime.petazzoni@bulix.org>

--- pandagen/plugins/transform.py
+++ pandagen/plugins/transform.py
@@ -13,6 +13,29 @@ class Markdown(pandagen.Plugin):
     is loaded into.
     """
 
+    class PictureExtension(markdown.extensions.Extension):
+        """A Markdown extension that detects paragraphs that only contain a
+        picture and annotates them with a special CSS class."""
+
+        def extendMarkdown(self, md, md_globals):
+            md.treeprocessors.add('p.img.class',
+                                  Markdown.PictureProcessor(md),
+                                  '_end')
+
+    class PictureProcessor(markdown.treeprocessors.Treeprocessor):
+
+        def run(self, root):
+            for child in root:
+                if self.matches(child):
+                    child.set('class', 'picture')
+                self.run(child)
+            return root
+
+        def matches(self, element):
+            return element.tag == 'p' and len(element) == 1 and \
+                    element.getchildren()[0].tag == 'img'
+
+
     def __init__(self, on='contents'):
         self.on = on
 
@@ -22,4 +45,6 @@ class Markdown(pandagen.Plugin):
                 logging.debug('Skipping Markdown tranform on missing ' +
                               'property `%s` of %s.', self.on, k)
                 continue
-            v[self.on] = markdown.markdown(v[self.on])
+            v[self.on] = markdown.markdown(
+                v[self.on], extensions=[Markdown.PictureExtension(),
+                                        'smartypants(entities=named)'])
