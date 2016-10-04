edffa13413ed31b943a9831eed8fcf64c56ee4c1
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2016-10-04 17:42:23 +0200

    Don't overwrite slug target if already present
    But provide the option to do so if desired.
    
    Signed-off-by: Maxime Petazzoni <maxime.petazzoni@bulix.org>

--- pandagen/plugins/slug.py
+++ pandagen/plugins/slug.py
@@ -10,10 +10,12 @@ class Slug(pandagen.Plugin):
     Slugify any item property into another.
     """
 
-    def __init__(self, key='title', into='slug'):
+    def __init__(self, key='title', into='slug', overwrite=False):
         self.key = key
         self.into = into
+        self.overwrite = overwrite
 
     def _execute(self, pg):
         for v in pg.data.values():
-            v[self.into] = slugify.slugify(unicode(v[self.key]))
+            if self.into not in v or self.overwrite:
+                v[self.into] = slugify.slugify(unicode(v[self.key]))
