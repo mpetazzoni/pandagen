9e67ef79c1da1a496d53b99e039896630c9d1f08
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2017-02-06 13:03:36 -0800

    A better attempt at serve+watch plugins
    Signed-off-by: Maxime Petazzoni <maxime.petazzoni@bulix.org>

--- pandagen/plugins/serve.py
+++ pandagen/plugins/serve.py
@@ -2,7 +2,6 @@
 
 import BaseHTTPServer
 import logging
-import os
 import SimpleHTTPServer
 import threading
 import time
@@ -15,14 +14,30 @@ class Serve(pandagen.Plugin):
     Serve the built site with SimpleHTTPServer (for development purposes).
     """
 
-    def __init__(self, port=8000, target='build'):
+    def __init__(self, port=8000, target='build', wait=False):
         self.port = port
         self.target = target
+        self.wait = wait
         self._httpd = None
 
+    def _get_address(self):
+        """Returns the listening address of the HTTP server, if running."""
+        assert self._httpd is not None
+        address = self._httpd.socket.getsockname()
+        return 'http://{0}:{1}/'.format(*address)
+
     def _execute(self, pg):
+        if self._httpd:
+            logging.info('Already serving at %s', self._get_address())
+            return
+
         t = threading.Thread(name='pandagen-httpd', target=self._serve)
+        t.daemon = True
         t.start()
+
+        if not self.wait:
+            return
+
         try:
             while t.is_alive():
                 time.sleep(0.5)
@@ -32,17 +47,29 @@ class Serve(pandagen.Plugin):
                 self._httpd.shutdown()
                 t.join()
 
+    class PathTranslatingHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        """
+        A request handler for the SimpleHTTPServer that translates incoming
+        requests to include the serving path into the mapped local file path.
+        For example, /index.html will read and return
+        <pandagen_target>/index.html.
+
+        <pandagen_target> is set from the serve target passed to the
+        constructor of the Serve plugin.
+        """
+
+        def translate_path(self, path):
+            path = '/{0}{1}'.format(self.server.pandagen_target, path)
+            return (SimpleHTTPServer.SimpleHTTPRequestHandler
+                    .translate_path(self, path))
 
     def _serve(self):
-        current = os.getcwd()
-        os.chdir(self.target)
         try:
             self._httpd = BaseHTTPServer.HTTPServer(
-                ('', self.port), SimpleHTTPServer.SimpleHTTPRequestHandler)
-            address = self._httpd.socket.getsockname()
-            logging.info('Serving at http://%s:%d/', *address)
+                ('', self.port), Serve.PathTranslatingHandler)
+            self._httpd.pandagen_target = self.target
+            logging.info('Serving %s at %s', self.target, self._get_address())
             self._httpd.serve_forever()
         finally:
-            os.chdir(current)
             self._httpd = None
             logging.info('Done serving.')
--- pandagen/plugins/watch.py
+++ pandagen/plugins/watch.py
@@ -1,6 +1,8 @@
 # Pandagen
 
+import fnmatch
 import logging
+import os.path
 import time
 import watchdog.events
 import watchdog.observers
@@ -9,9 +11,18 @@ from .. import pandagen
 
 
 class Watch(pandagen.Plugin, watchdog.events.FileSystemEventHandler):
+    """
+    Watch for files changes and re-execute the pipeline if a change is
+    detected. A list of excluded glob patterns can be specified via the exclude
+    paramater to the constructor. Make sure you exclude your build directory
+    (and anything underneath it); if you use Jinja2 templates, also exclude
+    anything under the .cache folder.
+    """
 
-    def __init__(self, location='src'):
+    def __init__(self, location='.',
+                 exclude=['.', '.cache/*', 'build', 'build/*', '*.swp']):
         self.location = location
+        self.exclude = [os.path.normpath(p) for p in exclude]
         self.observer = self._get_observer()
         self.pg = None
 
@@ -21,29 +32,33 @@ class Watch(pandagen.Plugin, watchdog.events.FileSystemEventHandler):
         return observer
 
     def on_any_event(self, event):
-        if not self.pg:
-            # Not yet executed/initialized; shouldn't happen
-            return
+        assert self.pg is not None
 
-        logging.info('Change detected. Rebuilding...')
-        self.observer.stop()
-        self.observer = self._get_observer()
+        for t in [watchdog.events.DirCreatedEvent,
+                  watchdog.events.DirModifiedEvent]:
+            if isinstance(event, t):
+                return
+
+        for exclude in self.exclude:
+            if fnmatch.fnmatch(os.path.normpath(event.src_path), exclude):
+                return
+
+        logging.info('Change detected in %s. Rebuilding...', event.src_path)
         self.pg.execute()
 
     def _execute(self, pg):
-        logging.warn('Watch plugin does not work yet; skipping...')
-        return
-
         self.pg = pg
         if self.observer.is_alive():
             logging.info('Watch already active.')
             return
 
-        logging.info('Starting watch of %s...', self.location)
+        logging.info('Starting watch of %s (excluding %s)...',
+                     self.location, ', '.join(self.exclude))
         self.observer.start()
         try:
             while self.observer.should_keep_running():
-                time.sleep(0.1)
+                time.sleep(1)
         except KeyboardInterrupt:
             self.observer.stop()
+        self.observer.join()
         logging.info('Done watching.')
