b95fa1772ad2c4e1d3532795071313e5fa216916
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2017-02-23 22:08:22 -0800

    Markdown transform fixes
    * Only add the 'picture' CSS class on paragraphs containing only an
      image if they truly only contain an image and no trailing text.
    * Add left/right CSS classes on images with that attribute, as extracted
      by the attr_list extension.
    * Output as HTML5

--- pandagen/plugins/transform.py
+++ pandagen/plugins/transform.py
@@ -13,30 +13,50 @@ class Markdown(pandagen.Plugin):
     is loaded into.
     """
 
-    class PictureExtension(markdown.extensions.Extension):
+    class PictureParagExtension(markdown.extensions.Extension):
         """A Markdown extension that detects paragraphs that only contain a
         picture and annotates them with a special CSS class."""
 
         def extendMarkdown(self, md, md_globals):
             md.treeprocessors.add('p.img.class',
-                                  Markdown.PictureProcessor(md),
+                                  Markdown.PictureParagProcessor(md),
                                   '_end')
 
-    class PictureProcessor(markdown.treeprocessors.Treeprocessor):
+    class PictureParagProcessor(markdown.treeprocessors.Treeprocessor):
 
         def run(self, root):
             for child in root:
                 if self.matches(child):
                     child.set('class', 'picture')
                 self.run(child)
-            return root
 
         def matches(self, element):
             return element.tag == 'p' and len(element) == 1 and \
-                    element.getchildren()[0].tag == 'img'
+                    element.getchildren()[0].tag == 'img' and \
+                    not element.getchildren()[0].tail
+
+    class PictureFloatExtension(markdown.extensions.Extension):
+        """A Markdown extension that sets the left/right CSS class on images
+        with that attribute (as extracted by the attr_list extension)."""
+
+        def extendMarkdown(self, md, md_globals):
+            md.treeprocessors.add('img.float',
+                                  Markdown.PictureFloatProcessor(md),
+                                  '_end')
+
+    class PictureFloatProcessor(markdown.treeprocessors.Treeprocessor):
+
+        def run(self, root):
+            for child in root:
+                if child.tag == 'img':
+                    side = child.get('left') or child.get('right')
+                    if side:
+                        child.set('class', side)
+                self.run(child)
 
     def __init__(self, on='contents'):
         self.on = on
+        logging.getLogger('MARKDOWN').setLevel(logging.INFO)
 
     def _execute(self, pg):
         for k, v in pg.data.items():
@@ -45,6 +65,10 @@ class Markdown(pandagen.Plugin):
                               'property `%s` of %s.', self.on, k)
                 continue
             v[self.on] = markdown.markdown(
-                v[self.on], extensions=[Markdown.PictureExtension(),
-                                        'markdown.extensions.smarty'])
+                    v[self.on], output_format='html5',
+                    extensions=[
+                        Markdown.PictureParagExtension(),
+                        Markdown.PictureFloatExtension(),
+                        'markdown.extensions.attr_list',
+                        'markdown.extensions.smarty'])
             logging.debug('Transformed %s:%s as Markdown.', k, self.on)
